33ba5f3558f66c2d3058ed9fcb5f41e5beca4fdc
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-21 00:00:50 +0200

    Provide HTTP Client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig , provideZoneChangeDetection } from '@angular/core';
 import {
-    provideRouter,
-    withHashLocation,
-    withInMemoryScrolling,
-    withViewTransitions,
+    provideRouter ,
+    withHashLocation ,
+    withInMemoryScrolling ,
+    withViewTransitions
 } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
-    provideClientHydration,
-    withEventReplay,
+    provideClientHydration ,
+    withEventReplay
 } from '@angular/platform-browser';
+import { provideHttpClient , withFetch } from '@angular/common/http';
 
-export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(
-            routes,
-            withInMemoryScrolling({
-                scrollPositionRestoration: 'top',
-            }),
-            withHashLocation(),
-            withViewTransitions()
-        ),
-        provideClientHydration(withEventReplay()),
-    ],
+export const appConfig : ApplicationConfig = {
+    providers : [
+        provideZoneChangeDetection ( { eventCoalescing : true } ) ,
+        provideRouter (
+            routes ,
+            withInMemoryScrolling ( {
+                scrollPositionRestoration : 'top'
+            } ) ,
+            withHashLocation () ,
+            withViewTransitions ()
+        ) ,
+        provideClientHydration ( withEventReplay () ) ,
+        provideHttpClient ( withFetch () )
+    ]
 };
