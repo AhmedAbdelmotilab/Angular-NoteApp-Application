8118df308fb4b0b6d962f5043ac251dc37f6ae2b
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-23 00:55:44 +0200

    Note Application Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes : Routes = [
+    {
+        path : '' ,
+        redirectTo : 'home' ,
+        pathMatch : 'full'
+    } ,
+    {
+        path : 'home' ,
+        loadComponent : () =>
+            import('./pages/home/home.component').then (
+                ( c ) => c.HomeComponent
+            ) ,
+        title : 'Home'
+    } ,
+    {
+        path : 'login' ,
+        loadComponent : () => import('./pages/login/login.component').then (
+            ( c ) => c.LoginComponent
+        ) ,
+        title : 'Login'
+    } ,
+    {
+        path : 'register' ,
+        loadComponent : () => import('./pages/register/register.component').then (
+            ( c ) => c.RegisterComponent
+        ) ,
+        title : 'Register'
+    } ,
+    {
+        path : '**' ,
+        loadComponent : () => import('./shared/components/not-found/not-found.component').then (
+            ( c ) => c.NotFoundComponent
+        ) ,
+        title : 'NotFoundPage'
+    }
+];
