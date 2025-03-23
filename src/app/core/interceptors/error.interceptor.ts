cacae057c0595e72e9272af275ac7808ebd5ec2a
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-24 01:44:39 +0200

    Improve The UI Of The Error Interceptor Using Sweat Alert 2

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,10 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { catchError , throwError } from 'rxjs';
+import Swal from 'sweetalert2';
 
 export const errorInterceptor : HttpInterceptorFn = ( req , next ) => {
     return next ( req ).pipe ( catchError ( ( err ) => {
-        // TODO : Add Toastr For Showing The Errors
-        console.log ( 'ErrorFromInterceptors' , err );
+        Swal.fire ( {
+            icon : 'error' ,
+            title : 'Oops...' ,
+            text : err.error.msg
+        } );
         return throwError ( () => err );
     } ) );
 };
