434dcc94f2b8d7e388d362e990bff62a4974ef5c
Ahmed Abdelmotilab <AhmedAbdelhakimAbdelmotilab@gmail.com>
2025-03-24 07:53:30 +0200

    Update NavBar With Logout Button And Show Links Based On The Login Or Not

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink , RouterLinkActive } from '@angular/router';
+import { Component , inject , OnInit , PLATFORM_ID } from '@angular/core';
+import { Router , RouterLink , RouterLinkActive } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
-@Component({
-  selector: 'app-navbar',
+@Component ( {
+    selector : 'app-navbar' ,
     imports : [
         RouterLink ,
         RouterLinkActive
-    ],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
-})
-export class NavbarComponent {
+    ] ,
+    templateUrl : './navbar.component.html' ,
+    styleUrl : './navbar.component.css'
+} )
+export class NavbarComponent implements OnInit {
+    private readonly platformId = inject ( PLATFORM_ID );
+    private readonly router = inject ( Router );
+    isLogIn : boolean = false;
 
+    ngOnInit () {
+        if ( isPlatformBrowser ( this.platformId ) ) {
+            if ( localStorage.getItem ( 'token' ) ) {
+                this.isLogIn = true;
+            }
+        }
+    }
+
+    logOut () : void {
+        if ( isPlatformBrowser ( this.platformId ) ) {
+            localStorage.removeItem ( 'token' );
+            this.router.navigate ( [ '/login' ] );
+        }
+    }
 }
